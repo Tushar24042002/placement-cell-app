5b56aed5c58aff5f9de29b06040550eaa512cf41
Tushar24042002 <sahilgupta24042002@gmail.com>
2023-08-06 09:16:18 +0530

    update

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
 require('./config/view-helpers')(app);
-const port = 8080;
-
+// const port = 8080;
+const port = process.env.PORT;
 const expressLayouts  = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const sassMiddleware = require('node-sass-middleware');
